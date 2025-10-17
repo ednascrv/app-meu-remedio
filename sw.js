9de1551b404c082bbb1f7dbd73038494f2fea5fb
Edna S. Carvalho <ednac7733@gmail.com>
2025-10-17 19:54:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+// receber notificações do app
+self.addEventListener('message', e => {
+  if(e.data.type === 'show-notification'){
+    self.registration.showNotification(e.data.title, e.data.opts);
+  }
+});
